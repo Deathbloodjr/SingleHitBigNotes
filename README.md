a2b32a62d113f6b0be2d3051d5bec5875b964276
Deathblood <deathbloodjr@gmail.com>
2023-05-29 09:33:17 -0500

    Added readme, changed patch code
    Previous method worked fine, new method is a lot simpler.

---
+++ README.md
@@ -0,0 +1,5 @@
+# SingleHitBigNotes
+ 
+This is a mod for Taiko no Tatsujin: The Drum Master!
+It allows you to only need to hit one side in order to hit big notes.
+It has support for Mono (1.2.2) and IL2CPP (1.3.0+) versions of the game.
--- SingleHitBigNotes/Patches/SingleHitBigNotesPatch.cs
+++ SingleHitBigNotes/Patches/SingleHitBigNotesPatch.cs
@@ -9,37 +9,21 @@ namespace SingleHitBigNotes.Patches
 {
     internal class SingleHitBigNotesPatch
     {
-		[HarmonyPatch(typeof(EnsoInput), "GetLastInputForCore")]
+		[HarmonyPatch(typeof(EnsoInput))]
+		[HarmonyPatch(nameof(EnsoInput.GetLastInputForCore))]
 		[HarmonyPatch(MethodType.Normal)]
-		[HarmonyPrefix]
-		public static bool EnsoInput_GetLastInputForCore_Prefix(EnsoInput __instance, ref TaikoCoreTypes.UserInputType __result, int player)
-		{
-			TaikoCoreTypes.UserInputType NewGetLastInputForCore()
-			{
-				EnsoInput.EnsoInputFlag flag = __instance.GetLastInput(player);
-				if (flag == EnsoInput.EnsoInputFlag.DonR ||
-					flag == EnsoInput.EnsoInputFlag.DonL ||
-					flag == EnsoInput.EnsoInputFlag.DaiDon)
-				{
-					flag = EnsoInput.EnsoInputFlag.DaiDon;
-				}
-				else if (flag == EnsoInput.EnsoInputFlag.KatsuR ||
-						 flag == EnsoInput.EnsoInputFlag.KatsuL ||
-						 flag == EnsoInput.EnsoInputFlag.DaiKatsu)
-				{
-					flag = EnsoInput.EnsoInputFlag.DaiKatsu;
-				}
-				var ensoParam = Traverse.Create(__instance).Field("ensoParam").GetValue() as EnsoPlayingParameter;
-				if (ensoParam.EnsoEndType == EnsoPlayingParameter.EnsoEndTypes.OptionPerfect || ensoParam.EnsoEndType == EnsoPlayingParameter.EnsoEndTypes.OptionTraining)
-				{
-					flag = EnsoInput.EnsoInputFlag.None;
-				}
-				return __instance.ToUserInputType(flag);
-			}
-
-			__result = NewGetLastInputForCore();
-
-			return false;
-		}
+		[HarmonyPostfix]
+		public static void EnsoInput_GetLastInputForCore_Postfix(EnsoInput __instance, ref TaikoCoreTypes.UserInputType __result, int player)
+        {
+            switch (__result)
+            {
+                case TaikoCoreTypes.UserInputType.Don_Pad:
+					__result = TaikoCoreTypes.UserInputType.Don_Strong;
+					break;
+                case TaikoCoreTypes.UserInputType.Katsu_Pad:
+					__result = TaikoCoreTypes.UserInputType.Katsu_Strong;
+                    break;
+            }
+        }
 	}
 }
