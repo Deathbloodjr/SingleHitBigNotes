c4064d6d38ddeb9fffddaed9fc2ca8dba308e3f1
Deathblood <deathbloodjr@gmail.com>
2023-05-29 09:42:12 -0500

    Fixed some tab/space formatting

--- SingleHitBigNotes/Patches/SingleHitBigNotesPatch.cs
+++ SingleHitBigNotes/Patches/SingleHitBigNotesPatch.cs
@@ -9,21 +9,21 @@ namespace SingleHitBigNotes.Patches
 {
     internal class SingleHitBigNotesPatch
     {
-		[HarmonyPatch(typeof(EnsoInput))]
-		[HarmonyPatch(nameof(EnsoInput.GetLastInputForCore))]
-		[HarmonyPatch(MethodType.Normal)]
-		[HarmonyPostfix]
-		public static void EnsoInput_GetLastInputForCore_Postfix(EnsoInput __instance, ref TaikoCoreTypes.UserInputType __result, int player)
+        [HarmonyPatch(typeof(EnsoInput))]
+        [HarmonyPatch(nameof(EnsoInput.GetLastInputForCore))]
+        [HarmonyPatch(MethodType.Normal)]
+        [HarmonyPostfix]
+        public static void EnsoInput_GetLastInputForCore_Postfix(EnsoInput __instance, ref TaikoCoreTypes.UserInputType __result, int player)
         {
             switch (__result)
             {
                 case TaikoCoreTypes.UserInputType.Don_Pad:
-					__result = TaikoCoreTypes.UserInputType.Don_Strong;
-					break;
+                    __result = TaikoCoreTypes.UserInputType.Don_Strong;
+                    break;
                 case TaikoCoreTypes.UserInputType.Katsu_Pad:
-					__result = TaikoCoreTypes.UserInputType.Katsu_Strong;
+                    __result = TaikoCoreTypes.UserInputType.Katsu_Strong;
                     break;
             }
         }
-	}
+    }
 }
